ac63ba9f51576813e04ee3f571fc8aeab487837d
vy <vy251028@gmail.com>
2026-07-27 19:16:02 +0700

    Them Morgan logger va cau hinh Prettier

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // index.js - File khởi tạo server chính của dự án Coffee Shop
 
-const express = require('express');
+const express = require("express");
+const morgan = require("morgan"); // Ghi log các request gửi lên server
+
 const app = express();
 const port = 3000;
 
-// Route mặc định - trang chủ
-app.get('/', (req, res) => {
-  res.send('Website bán hạt cà phê!');
+// Tích hợp morgan để log mọi request (GET, POST...) ra Terminal
+app.use(morgan("combined"));
+
+app.get("//", (req, res) => {
+  res.send("Website bán hạt cà phê - Đồ án nhóm");
 });
 
 app.listen(port, () => {
